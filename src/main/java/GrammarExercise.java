abb06693dc2eb6a78dd847115e7656ad59be9b61
Liangliang Liu <max1141847753@gmail.com>
2020-07-22 10:19:11 +0800

    [Liangliang Liu] 完成GrammarExerciseTest

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,8 @@
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -6,13 +10,59 @@ public class GrammarExercise {
         String firstWordList = "";
         String secondWordList = "";
 
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+        List<String> result = findCommonWordsWithSpace(firstWordList, secondWordList);
         //按要求输出到命令行
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
+
+        firstWordList = firstWordList.toUpperCase();
+        secondWordList = secondWordList.toUpperCase();
+
+        // 解析字符串
+        String[] firstSplitString = firstWordList.split(",");
+        String[] secondSplitString = secondWordList.split(",");
+
+
+        Stream.of(firstSplitString).forEach(GrammarExercise::checkString);
+
+
+        Stream.of(secondSplitString).forEach(GrammarExercise::checkString);
+
+        Set<String> firstStringSet = Stream.of(firstSplitString).collect(Collectors.toSet());
+        Set<String> secondStringSet = Stream.of(secondSplitString).collect(Collectors.toSet());
+
+
+        firstStringSet.retainAll(secondStringSet);
+
+
         //在这编写实现代码
-        return null;
+        return firstStringSet.stream().sorted().map(GrammarExercise::addSpace).collect(Collectors.toList());
     }
+
+    private static String addSpace(String s) {
+        char[] chars = s.toCharArray();
+
+        StringBuilder stringBuffer = new StringBuilder();
+        for (int i = 0; i < chars.length - 1; i++) {
+            stringBuffer.append(chars[i]).append(" ");
+        }
+        stringBuffer.append(chars[chars.length - 1]);
+        return stringBuffer.toString();
+    }
+
+    private static void checkString(String string) {
+        if ("".equals(string) || !checkIllegal(string)) {
+            throw new RuntimeException("input not valid");
+        }
+
+    }
+
+    private static boolean checkIllegal(String string) {
+        String pattern = "^[A-Z]+$";
+        return Pattern.matches(pattern, string);
+    }
+
+
 }
